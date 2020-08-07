3d98921e362b5b0e4eeb606c99b5f82401a0feee
abemin <muhaimifatihi@hotmail.com>
2020-08-07 15:25:18 +0800

    Update ssl-scan.sh

--- ssl-scan.sh
+++ ssl-scan.sh
@@ -1,13 +1,47 @@
 #! /bin/bash
 
-while getopts h:o: flag
-	do
-	    case "${flag}" in
-	        h) host=${OPTARG};;
-	        o) output=${OPTARG};;
-	    esac
-	done
+################################################################################
+# Help                                                                         #
+################################################################################
+Help()
+{
+   # Display Help
+   echo "Add description of the script functions here."
+   echo
+   echo "Syntax: ./ssl-scan.sh [ -h | i | o ]"
+   echo "options:"
+   echo "h     Print this Help."
+   echo "i     host name."
+   echo "o     output filename."
+   echo
+}
 
-python2 -m sslyze --regular --xml_out=/dev/stdout --quiet $host:443 | xsltproc sslyze.xsl - > $output.html
+################################################################################
+################################################################################
+# Main program                                                                 #
+################################################################################
+################################################################################
+
+while getopts ":h" option; do
+   case $option in
+      h) # display Help
+         Help
+         exit;;
+      i) hostname=${OPTARG};;
+      o) output=${OPTARG};;
+     \?) # incorrect option
+         echo "Error: Invalid option"
+         exit;;
+   esac
+done
+
+echo "   __________ __        _____ _________    _   __"
+echo "  / ___/ ___// /       / ___// ____/   |  / | / /"
+echo "  \__ \\__ \/ /  ______\__ \/ /   / /| | /  |/ / "
+echo " ___/ /__/ / /__/_____/__/ / /___/ ___ |/ /|  /  "
+echo "/____/____/_____/    /____/\____/_/  |_/_/ |_/   "
+                                                 
+
+python2 -m sslyze --regular --xml_out=/dev/stdout --quiet $hostname:443 | xsltproc sslyze.xsl - > $output.html
 
 echo "Scan Done!";
